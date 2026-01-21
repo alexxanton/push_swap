9084d0de8ccd224fce10fb4e56b5408a0736d5e3
Alex Anton Aranda <aanton-a@car15s1.42barcelona.com>
2026-01-21 17:30:10 +0100

    added hardcoded function for 3 number sorting

--- src/sort/sort.c
+++ src/sort/sort.c
@@ -6,11 +6,43 @@
 /*   By: aanton-a <aanton-a@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 11:26:07 by aanton-a          #+#    #+#             */
-/*   Updated: 2026/01/21 13:30:05 by aanton-a         ###   ########.fr       */
+/*   Updated: 2026/01/21 15:27:40 by aanton-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <../include/push_swap.h>
+
+int	sort_three_nums(t_list *stack)
+{
+	int		nums[3];
+	int		i;
+
+	i = 0;
+	while (i < 3)
+	{
+		nums[i] = stack->num;
+		if (stack->next)
+			stack = stack->next;
+		i++;
+	}
+	if (nums[0] > nums[1] && nums[1] < nums[2])
+		ra(&stack);
+	else if (nums[0] > nums[1] && nums[1] > nums[2])
+	{
+		ra(&stack);
+		sa(&stack);
+	}
+	else if (nums[0] < nums[1] && nums[1] > nums[2] && nums[0] < nums[2])
+	{
+		sa(&stack);
+		ra(&stack);
+	}
+	else if (nums[0] < nums[1] && nums[1] > nums[2] && nums[0] > nums[2])
+	{
+		rra(&stack);
+	}
+	return (1);
+}
 
 int	get_next_move(t_list **a, t_list **b)
 {
@@ -26,6 +58,7 @@ int	sort(t_list **a)
 
 	b = NULL;
 	count = 0;
+	return (sort_three_nums(*a));
 	while ((*a && !is_sorted(*a)) || b)
 	{
 		count += get_next_move(a, &b);
